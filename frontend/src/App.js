df5b5cfdba759be54e413cc613666092d66cc2d9
arinalupusor <lupusorarina@gmail.com>
2023-10-24 22:53:41 +0300

    Backend and FrontEnd initialisation finished.

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import About  from "./pages/About"
+import Home  from "./pages/Home"
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+      
+export default App
+
+
+
+
+
