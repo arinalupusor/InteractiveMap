404a5f7257b815d6531c29ecad4c066c8ee08ad2
Daniela3125 <danielamalos895@yahoo.com>
2023-10-24 21:08:34 +0300

    Adaugat

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import  About  from "./components/About"
+import Home  from "./components/Home"
+import LoginComponent from "./LoginComponent";
+import RegisterComponent from "./RegisterComponent";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Hello World</h1>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route path="/login" element={LoginComponent} />
+      <Route path="/register" element={RegisterComponent} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Switch>
+  </BrowserRouter>
+  )
 }
+      
+export default App
 
-export default App;
+
+
+
+
