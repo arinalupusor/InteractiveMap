fce2265ac560ba9730ca5ac4695e5b8b877da0c4
Steftrix <zaborilastefan5@gmail.com>
2023-11-04 17:44:06 +0200

    Starter API de CRUD locuri interes

---
+++ backend/pins/src/main/java/com/crud/pins/PinsApplication.java
@@ -0,0 +1,13 @@
+package com.crud.pins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinsApplication.class, args);
+	}
+
+}
