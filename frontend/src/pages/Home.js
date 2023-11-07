5bef1a20dcbc323ffb0806b89a63c7ea6598335d
imdamy1 <vega_damian2002@yahoo.com>
2023-11-07 20:02:52 +0200

    INITIAL

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,120 +1,125 @@
-import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import {Helmet} from "react-helmet";
+import "../Pinn.css";
+ 
+import image1 from "../images/image1.png";
+import image2 from "../images/image2.png";
+import image3 from "../images/image3.png";
 
 
+ 
 const MapContainer = () => {
-    const mapStyles = {
-      height: "400px",
-      width:"90%",
-      position:"relative",
-      top:"25px",
-      left:"10px",
-
-    };
-  
-  
-    const defaultCenter = {
-      lat: 47.1585,
-      lng: 27.6014
-    };
-  
-    const locations = [
-      {
-        name: "Playground",
-        location: {
-          lat: 40.712776,
-          lng: -74.005974
-        },
-        type: "playground"
-      },
-      {
-        name: "Current Event",
-        location: {
-          lat: 40.73061,
-          lng: -73.935242
-        },
-        type:"current"
-      },
-      {
-        name:"Future event",
-        location: {
-            lat: 40.75,
-            lng: -74.2
-          },
-          type:"future"
-      }
-    ];
-  
-    return (
-      <LoadScript googleMapsApiKey="AIzaSyBlbdzY6NZd1trSEri0pV7pzgiy3Wkag6U">
-        <GoogleMap
-          mapContainerStyle={mapStyles}
-          zoom={10}
-          center={defaultCenter}
-        >
-         {locations.map((item, index) => {
-          let pinColor = "purple"; 
-
-          if (item.type === "playground") {
-            pinColor = "green"; 
-          } else if (item.type === "current") {
-            pinColor = "orange"; 
-          } else if (item.type === "future") {
-            pinColor = "purple"; 
-          }
-            return (
-              <Marker
-                key={index}
-                position={item.location}
-                label={item.name}
-                icon={{
-                    url: `https://maps.google.com/mapfiles/ms/icons/${pinColor}-dot.png`, // Adaugă culoarea specificată la URL-ul icoanei pinului
-                  }}
-              />
-            );
-          })}
-          </GoogleMap>
-          </LoadScript>
-    );
+  const mapStyles = {
+    height: "400px",
+    width: "90%",
+    position: "relative",
+    top: "25px",
+    left: "10px",
   };
-  const UpcomingEvents = ({ upcomingEvents }) => (
-    <div className="upcoming-events">
-        <div className="events-box">
-            <h2>Upcoming Events</h2>
-            <button className="add-event-button">Add Event</button>
-            <ul>
-                {upcomingEvents.map((event, index) => (
-                    <li key={index}>{event}</li>
-                ))}
-            </ul>
+ 
+  const defaultCenter = {
+    lat: 47.1585,
+    lng: 27.6014,
+  };
+ 
+  const locations = [
+      {
+        name: 'Marker 1',
+        location: {
+          lat: 47.151885082487524,
+          lng: 27.581064364582836
+        },
+        icon: 'url_imaginesauculoare',
+        id: 'marker1'
+      }
+
+    //   {
+    //     name: 'Marker 2',
+    //     location: {
+    //       lat: 47.14395421490899,
+    //       lng: 27.582079811929617,
+    //     }, 
+    //     icon: 'url_imaginesauculoare2',
+    //     id: 'marker2',
+    //   },
+    //   {
+    //     name: 'Marker 3',
+    //     location: {
+    //       lat: 47.16170746357623, 
+    //       lng: 27.612238821160567,
+    //     },
+    //     icon: 'url_imaginesauculoare3',
+    //     id: 'marker3',
+    //   },
+    //   // Adaugă mai multe obiecte pentru fiecare marker adițional
+    ];
+      
+
+
+
+  return (
+    <div>
+      <Helmet>
+    <LoadScript googleMapsApiKey="AIzaSyBlbdzY6NZd1trSEri0pV7pzgiy3Wkag6U">
+
+    </LoadScript>
+    </Helmet>
+      <GoogleMap mapContainerStyle={mapStyles} zoom={10} center={defaultCenter}>
+        {locations.map((location, index) => (
+        
+<Marker
+key={index}
+position={location.location}
+      label= 'Marker 1'
+      icon={{
+        url: image1,
+        scaledSize: new window.google.maps.Size(30,40)
+      }}
+    />
+  ))}
+
+
+        </GoogleMap>
+
+         
+
         </div>
-        <div className="image-container">
-        <img
-            className="hot-air-balloon-icon"
-            src="https://img.icons8.com/external-microdots-premium-microdot-graphic/64/external-air-transportation-vol2-microdots-premium-microdot-graphic.png"
-            alt="external-air-transportation-vol2-microdots-premium-microdot-graphic"
-        />
-        </div>
-        </div>
-    
+  );
+};
+ 
+const UpcomingEvents = ({ upcomingEvents }) => (
+  <div className="upcoming-events">
+    <div className="events-box">
+      <h2>Upcoming Events</h2>
+      <div className="button-image-container">
+      </div>
+      <ul>
+        {upcomingEvents.map((event, index) => (
+          <li key={index}>{event}</li>
+        ))}
+      </ul>
+    </div>
+  </div>
 );
+ 
 const Home = () => {
   const upcomingEvents = [
-      "Event 1",
-      "Event 2",
-      "Event 3",
-      "Event 4",
-      "Event 5",
+    "Event 1",
+    "Event 2",
+    "Event 3",
+    "Event 4",
+    "Event 5",
   ];
+ 
   return (
     <div className="home-container">
-        <MapContainer />
-        <UpcomingEvents upcomingEvents={upcomingEvents} />
-         </div>
-    
-  )
+      <MapContainer />
+      <UpcomingEvents upcomingEvents={upcomingEvents} />
+    </div>
+  );
 };
-
+ 
 export default Home;
   
 
