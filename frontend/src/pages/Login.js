30a79b9a3c6df0a4c3eb70fe9f0c323a7a69f52e
Tozlovanu Sandu <Sandu.TOZLOVANU@geminicad.com>
2023-11-08 19:57:06 +0200

    Stefan - Login + Register integration + Branch Integration

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,10 +1,35 @@
 import React, { useState } from 'react';
-import { TiUser, TiMail,  TiKey } from 'react-icons/ti'; 
+import { TiUser, TiMail,  TiKey } from 'react-icons/ti';
+import axios from 'axios';
+import config from "../config.json";
+
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const handleLogin = () => {
-        
+    const [errorText, setErrorText] = useState('');
+    const handleLogin = () =>
+    {
+        if(email.length < 5 || password.length < 1)
+        {
+            setErrorText("Email or password are missing or are too short")
+            return
+        }
+
+        axios.post(config.url + '/api/v1/auth/authenticate', {
+            email: email,
+            password: password
+        })
+            .then(response => {
+                // Handle successful login
+                console.log(response.data);
+                localStorage.setItem('token', response.data.token);
+            })
+            .catch(error => {
+                // Handle error
+                let msg = 'Error authenticating'
+                console.log(msg);
+                setErrorText(msg)
+            });
     };
 
 
