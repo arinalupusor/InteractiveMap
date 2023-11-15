b98906812d14ecdda00f7e11a380e8066a9e8008
Daniela3125 <danielamalos895@yahoo.com>
2023-11-15 13:21:16 +0200

    Middleware

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,34 @@
-import {Route, Routes, BrowserRouter} from 'react-router-dom';
-import About  from "./pages/About"
-import Home  from "./pages/Home"
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Header from "./components/Header";
+import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import LeafletMap from './components/LeafletMap';
+
+import About from './pages/About';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
-import "./Register.css";
-import "./Login.css";
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './path-to-reducer/reducer';
-import myMiddleware from './path-to-middleware/middleware';
+import './Register.css';
+import './Login.css';
 
 function App() {
   return (
     <div className="app">
-      <Provider store={store}>
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </Router>
-      </Provider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/about" element={<About />} />
+          
+          <Route path="/" element={<Home />} />
+          <Route path="/map" element={<LeafletMap />} /> {/* Noua rută pentru LeafletMap */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
-
 
 export default App;
