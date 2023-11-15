fdc0a691e5d90be33ce4a015c4330d9284b44dba
Steftrix <zaborilastefan5@gmail.com>
2023-11-15 15:36:45 +0200

    Login endpoint refactor

--- backend/root/src/main/java/com/backend/root/TestHarta/Auth/AuthenticationResponse.java
+++ backend/root/src/main/java/com/backend/root/TestHarta/Auth/AuthenticationResponse.java
@@ -1,5 +1,6 @@
 package com.backend.root.TestHarta.Auth;
 
+import com.backend.root.TestHarta.User.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,4 +13,6 @@ import lombok.NoArgsConstructor;
 public class AuthenticationResponse {
 
     private String token;
+    private String email;
+    private Role type;
 }
