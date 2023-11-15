e0a7ee808b8c6d7758f8a8bec4ba744edc11abc5
Tozlovanu Sandu <Sandu.TOZLOVANU@geminicad.com>
2023-11-15 17:02:03 +0200

    Email and accounttype added to login ( they came null before this commit)

--- backend/root/src/main/java/com/backend/root/TestHarta/Auth/AuthenticationService.java
+++ backend/root/src/main/java/com/backend/root/TestHarta/Auth/AuthenticationService.java
@@ -24,14 +24,14 @@ public class AuthenticationService {
     var user = AppUser.builder().firstname(request.getFirstname()).lastname(request.getLastname()).email((request.getEmail())).password(passwordEncoder.encode(request.getPassword())).role(Role.USER).build();
     repository.save(user);
     var jwtToken = jwtService.generateToken(user);
-    return AuthenticationResponse.builder().token(jwtToken).build();
+    return AuthenticationResponse.builder().token(jwtToken).email(user.getEmail()).type(user.getRole()).build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
     authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
     var user = repository.findByEmail(request.getEmail()).orElseThrow();
         var jwtToken = jwtService.generateToken(user);
-        return AuthenticationResponse.builder().token(jwtToken).build();
+        return AuthenticationResponse.builder().token(jwtToken).email(user.getEmail()).type(user.getRole()).build();
     }
 
 }
