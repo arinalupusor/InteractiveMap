13247e1e1fd137cc089e8f02e10c6dba35f3de1b
Tozlovanu Sandu <Sandu.TOZLOVANU@geminicad.com>
2023-11-21 11:28:44 +0200

    Repaired bug on when you could register multiple time with the same email.

--- backend/root/src/main/java/com/backend/root/TestHarta/Auth/AuthenticationService.java
+++ backend/root/src/main/java/com/backend/root/TestHarta/Auth/AuthenticationService.java
@@ -21,15 +21,16 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
-    var user = AppUser.builder().firstname(request.getFirstname()).lastname(request.getLastname()).email((request.getEmail())).password(passwordEncoder.encode(request.getPassword())).role(Role.USER).build();
-    repository.save(user);
-    var jwtToken = jwtService.generateToken(user);
-    return AuthenticationResponse.builder().token(jwtToken).email(user.getEmail()).type(user.getRole()).build();
+        repository.findByEmail(request.getEmail()).ifPresent(user -> {throw new RuntimeException();});
+        var user = AppUser.builder().firstname(request.getFirstname()).lastname(request.getLastname()).email((request.getEmail())).password(passwordEncoder.encode(request.getPassword())).role(Role.USER).build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).email(user.getEmail()).type(user.getRole()).build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
-    var user = repository.findByEmail(request.getEmail()).orElseThrow();
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+        var user = repository.findByEmail(request.getEmail()).orElseThrow();
         var jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder().token(jwtToken).email(user.getEmail()).type(user.getRole()).build();
     }
